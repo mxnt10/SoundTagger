c35982e0c5f387d43b45e092b674185bc64c34f0
mxnt10 <m10ferrari1200@gmail.com>
2023-07-30 18:12:04 -0300

    add parent

--- src/hboxlayout.py
+++ src/hboxlayout.py
@@ -3,9 +3,9 @@ from PyQt5.QtWidgets import QHBoxLayout
 
 # Customização do layout para personalizar a interface mais facilmente
 class HBoxLayout(QHBoxLayout):
-    def __init__(self, margin=5, array_widgets=None):
+    def __init__(self, margin=5, array_widgets=None, parent=None):
         self.n = margin
-        super().__init__()
+        super().__init__(parent)
         self.setContentsMargins(self.n, self.n, self.n, self.n)
 
         if array_widgets is not None:
