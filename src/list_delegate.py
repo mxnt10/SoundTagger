dc6ff71d207a0212bc014b4f2600db28012346a4
mxnt10 <m10ferrari1200@gmail.com>
2023-07-30 18:12:31 -0300

    change style

--- src/list_delegate.py
+++ src/list_delegate.py
@@ -1,12 +1,20 @@
+from PyQt5.QtGui import QPalette
 from PyQt5.QtWidgets import QStyledItemDelegate, QStyle
-
-from list_enum import __FILES__
 
 
 # Delegate para impedir a seleção e o realce de colunas específicas
 class ListDelegate(QStyledItemDelegate):
+    def initStyleOption(self, option, index):
+        super().initStyleOption(option, index)
+
+        # Definir a cor da seleção
+        if option.state & QStyle.State_Selected:
+            palette = option.palette
+            highlight_color = palette.color(QPalette.Highlight)
+            highlight_color.setAlpha(100)
+            palette.setColor(QPalette.Highlight, highlight_color)
+            option.palette = palette
+
     def paint(self, painter, option, index):
         option.state &= ~QStyle.State_MouseOver
-        if index.column() != __FILES__:
-            option.state &= ~QStyle.State_Selected
         super().paint(painter, option, index)
